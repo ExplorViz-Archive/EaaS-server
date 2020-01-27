3598c3524df58092d63e8920b76b7fa249435044
Jan Erik Petersen <JanErikPetersen@web.de>
2020-01-27 23:41:50 +0100

    Remove GROUP_ID_DOCKER variable, find id ourselves
    Having to set this variable by hand is tedious and makes it hard to deploy EaaS
    automatically. We can just take the owner group id of the socket file.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 #shellcheck shell=ash
-set -eo pipefail
+set -euo pipefail
 
-# Allow our eaas user to access the docker socket on the host
-if [ -n "$GROUP_ID_DOCKER" ]; then
-    if getent group "$GROUP_ID_DOCKER" >/dev/null; then
-        # delete whatever useless group occupies the ID we need
-        # laughable but true, busybox can't delete a group by its gid
-        delgroup "$(getent group "$GROUP_ID_DOCKER" | cut -d: -f1)"
+DOCKER_SOCKET="/var/run/docker.sock"
+
+# Allow our eaas user to access the docker socket on the host without root
+if [ -e "$DOCKER_SOCKET" ]; then
+    gid="$(stat -c '%g' "$DOCKER_SOCKET")"
+    echo "[entrypoint] Found $DOCKER_SOCKET, owned by group id $gid" >&2
+
+    if getent group "$gid" >/dev/null; then
+        gname="$(getent group "$gid" | cut -d: -f1)"
+        echo "[entrypoint] Group id belongs to group $gname. Deleting it" >&2
+        delgroup "$gname"
     fi
 
-    addgroup -g "$GROUP_ID_DOCKER" docker
+    addgroup -g "$gid" docker
     addgroup eaas docker
 fi
-
-unset GROUP_ID_DOCKER
 
 exec su-exec eaas "$@"
