6949f747c3ea37195cf9133d55a2e12296955f93
Jan Erik Petersen <JanErikPetersen@web.de>
2020-02-10 04:30:55 +0100

    Make error pages public, add debug output
    - Some errors views like missing routes right now trigger the /login
      redirect for no reason
    - Log all views about to be entered on debug level
    - Add example log level options to application.properties

--- src/main/java/net/explorviz/eaas/security/UIEnterAuthenticator.java
+++ src/main/java/net/explorviz/eaas/security/UIEnterAuthenticator.java
@@ -2,23 +2,36 @@ package net.explorviz.eaas.security;
 
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterListener;
+import com.vaadin.flow.router.HasErrorParameter;
+import lombok.extern.slf4j.Slf4j;
 import net.explorviz.eaas.frontend.view.LoginView;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.annotation.Secured;
 
 /**
- * Reroutes the user to the {@link LoginView} if they're not logged in. Also checks for {@link Secured} annotations
- * on views about to be entered and checks if the current security context has the necessary authority.
+ * Checks if the current security context has the necessary permission before entering views.
+ * <p>
+ * E.g. it reroutes the user to the {@link LoginView} if they're not logged in. Also checks for {@link Secured}
+ * annotations on views about to be entered and checks if the current security context has the necessary authority.
  *
  * @see <a href="https://vaadin.com/tutorials/securing-your-app-with-spring-security/setting-up-spring-security">Setting
  * up Spring Security for Vaadin applications</a>
  */
+@Slf4j
 public class UIEnterAuthenticator implements BeforeEnterListener {
     private static final long serialVersionUID = 8601163001283809803L;
 
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
         Class<?> target = event.getNavigationTarget();
+
+        log.debug("About to enter view {}", target.getCanonicalName());
+
+        // Error pages can be public
+        if (HasErrorParameter.class.isAssignableFrom(target)) {
+            log.debug("Allow entering view {} because it is an error page", target.getCanonicalName());
+            return;
+        }
 
         // TODO: Allow a public front page
         if (!SecurityUtils.isUserLoggedIn() && !LoginView.class.equals(target)) {
